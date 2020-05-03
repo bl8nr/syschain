04006d1cd0b6ad1dd3fda4ec8e448fbf5d2e043d
Brett Bloethner <brett@Bretts-MBP.localdomain>
2020-05-03 13:37:49 -0600

    clean up the contracts even though they wont be used this time around

---
+++ src/contracts/storeLogDigest/storeLogDigest.sol
@@ -0,0 +1,10 @@
+/**
+    this contract is unused right now since i opted to use
+    transactions for this project iteration, for added simplicity
+ */
+
+pragma solidity >=0.4.0 <0.7.0;
+
+contract StoreLogDigest {
+    
+}
